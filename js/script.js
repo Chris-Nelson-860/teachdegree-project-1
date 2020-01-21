a4120c6bcfb5600c93edac5f16f2044fa06caa78
Chris-Nelson-860 <cpnelson222@gmail.com>
2020-01-20 19:12:13 -0500

    created the function to print out the random quote

--- js/script.js
+++ js/script.js
@@ -13,56 +13,71 @@ project 1 - A Random Quote Generator
 var quotes = [
   {
     quote: "You can fail at what you don't want, so might as well take a chance on doing what you love",
-    author: "Jim Carrey"
+    source: "Jim Carrey",
+    citation: "Maharishi University Commencement",
+    year: "2014"
   },
   {
     quote: "Consistency will knock the sh*t out of motivation every time",
-    author: "Mark Bell"
+    source: "Mark Bell"
   },
   {
     quote: "Sleep faster",
-    author: "Arnold Schwarzenegger"
+    source: "Arnold Schwarzenegger",
   },
   {
     quote: "I learned that if you work hard and creatively, you can have just about anything you want, but not everything you want. Maturity is the ability to reject good alternatives in order to pursue even better ones",
-    author: "Ray Dalio"
+    source: "Ray Dalio",
+    citation: "Principles- life and work",
+    year: "2019"
   },
   {
     quote: "Fatigue is the best pillow",
-    author: "Benjamin Franklin"
+    source: "Benjamin Franklin"
   },
   {
     quote: "Being realistic is the most commonly traveled road to mediocrity",
-    author: "Will Smith"
+    source: "Will Smith"
   },
   {
     quote: "There’s no talent here, this is hard work. This is an obsession. Talent does not exist, we are all human beings. You could be anyone if you put in the time. You will reach the top, and that’s that. I am not talented, I am obsessed.",
-    author: "Conor McGregor"
+    source: "Conor McGregor"
   }
 ]
+
+//Set some variables for the getRandomQuoteFunction
+var theQuote;
+var theSource;
+var theTitle;
+var theYear;
+var quoteBox = document.getElementById("quote-box");
+var messageToPrint ='';
+
 
 
 /***
  * `getRandomQuote` function
 ***/
 
-//First I am going to generate a random number that will not be greater than the length of the array of quotes.
-function getRandomInt(max){
-  return Math.floor(Math.random() * Math.floor(max));
+
+function getRandomQuote(){
+  var randomNumber = Math.floor(Math.random() * (quotes.length - 1));
+  theQuote = quotes[randomNumber].quote;
+  theSource = quotes[randomNumber].source;
+  return theQuote;
 }
-var randomNumber = getRandomInt(quotes.length - 1);
-
-console.log(quotes.length);//TEST
-console.log(randomNumber);//TEST
-
-
+console.log(getRandomQuote());
 
 
 /***
  * `printQuote` function
 ***/
-
-
+function printQuote(){
+  messageToPrint += '<p class="quote">"' + theQuote + '</p>';
+  messageToPrint += '<p class="source">' + theSource + '</p>'; 
+  quoteBox.innerHTML = messageToPrint;
+}
+//console.log(messageToPrint);
 
 /***
  * click event listener for the print quote button
